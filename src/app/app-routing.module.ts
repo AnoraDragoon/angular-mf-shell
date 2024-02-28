c4d42f5a293e5b4928aa9322961dd849a86704c7
AnoraDragoon <anoradragoon@gmail.com>
2024-02-28 21:16:31 +0100

    Module Federation with Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './web/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        pathMatch: 'full'
+    },
+    {
+        path: 'flights',
+        loadChildren: () => import('mfe1/Module').then(m => m.FlightsModule)
+    },
+    {
+        path: 'remote',
+        loadChildren: () => import('remote/Module').then(m => m.TestRemoteModule)
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
